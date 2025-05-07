90283d45dee26d1890f542579f0ca2cda817d86a
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-05-07 09:43:01 +0530

    Create user repo and modify it

--- backend/src/main/java/lk/artify/backend/repository/UserRepository.java
+++ backend/src/main/java/lk/artify/backend/repository/UserRepository.java
@@ -13,5 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
 
-    Optional<User> findByUsernameOrEmailAndPassword(String username, String email, String password);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByEmail(String email);
 }
